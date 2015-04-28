4e65a3ffbaf1548e2e65f3cc75249d43a4270455
gumbee <mugeeb.hassan@gmail.com>
2015-04-29 00:09:48 +0200

    added SQLAlchemy

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
 from app import app
+from app import db
+from app.models import User
+from flask.ext.sqlalchemy import SQLAlchemy
+import logging
 
-app.run(debug = True)
+db.create_all()
+app.run(debug = True, use_reloader = False)
